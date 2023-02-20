b39f650c39b58626a4095fb4cd0122a5f67503f3
Arindam Saha <asaha15071998@gmail.com>
2023-02-20 19:15:28 +0530

    Added Feauture to remove a word

--- src/Hash/HashMain.java
+++ src/Hash/HashMain.java
@@ -9,7 +9,8 @@ public class HashMain {
 
         System.out.println("---Word Frequency Calculator Program---");
         FrequencyCalculator mapString = new FrequencyCalculator();
-        System.out.println("Input Sentence : " + "Paranoids are not\n" +
+
+        System.out.println("Input Paragraph : " + "Paranoids are not\n" +
                                                  "paranoid because they are paranoid but\n" +
                                                  "because they keep putting themselves\n" +
                                                  "deliberately into paranoid avoidable\n" +
@@ -22,6 +23,9 @@ public class HashMain {
                                         "deliberately into paranoid avoidable" +
                                         "situations");
 
+        System.out.println();
+
+        mapString.removeWord("Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations", "avoidable");
     }
 }
 
@@ -44,7 +48,27 @@ class FrequencyCalculator {
 
         System.out.println(value);
     }
-}
 
+    public void removeWord(String input, String delete) {
+
+        Hashtable<String, Integer> value = new Hashtable<>();
+        String[] words = input.split(" ");
+
+
+        for (String word : words) {
+
+            Integer val = value.get(word);
+            if (val == null) {
+                value.put(word, 1);
+            } else {
+                value.put(word, val + 1);
+            }
+        }
+        System.out.println(value);
+        System.out.println("After Delete");
+        value.remove(delete);
+        System.out.println(value);
+    }
+}
 
 
