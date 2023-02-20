a74727441f63684d162823410a6c3065f579a932
Arindam Saha <asaha15071998@gmail.com>
2023-02-20 18:30:10 +0530

    Welcome Main

---
+++ src/Hash/HashMain.java
@@ -0,0 +1,13 @@
+package Hash;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+
+public class HashMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("---Word Frequency Calculator Program---");
+    }
+}
+
